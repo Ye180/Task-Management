8a4690a410255ec6cdd5804202a2ff81742f1cdd
Emmanuel252 <ouattarayoussoufemmanuel@gmail.com>
2025-03-09 19:43:18 +0000

    Setting Up a databases

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { TasksModule } from './tasks/tasks.module';
 @Module({
-    imports: [TasksModule],
+    imports: [
+        TasksModule,
+        TypeOrmModule.forRoot({
+            type: 'postgres',
+            host: 'localhost',
+            port: 5432,
+            username: 'postgres',
+            password: 'postgres',
+            database: 'task-management',
+            autoLoadEntities: true, //If true, entities will be loaded automatically.
+            synchronize: true, //If true, it will automatically create database tables and columns.
+        }),
+    ],
 })
 export class AppModule {}
